4199ba1e38b557b281aad8367a66e3ef872533cc
ElBe <90863907+ElBe-Plaq@users.noreply.github.com>
2023-01-16 11:19:57 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import discord
 import os
 from discord.ext import commands
 
-TOKEN = "You're bot token"
-PREFIX = "You're bot prefix"
+TOKEN = "Your bot token"
+PREFIX = "Your bot prefix"
 
 bot = commands.AutoShardedBot(command_prefix=PREFIX)
 bot.remove_command('help')
